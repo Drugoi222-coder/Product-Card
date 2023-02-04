66d67c6b54c23b24d7cabc4665d9a919078b0fe3
Bogdan Gordin <bogdan_g@inbox.ru>
2023-02-04 21:14:18 +0300

    feat: gallery particulary scripted

--- script/index.js
+++ script/index.js
@@ -6,18 +6,35 @@ window.addEventListener("DOMContentLoaded", () => {
     const minus = document.querySelector(".add-to-cart__minus");
     const addBtn = document.querySelector(".add-to-cart__add-btn");
     const goodsList = document.querySelector(".goods__list");
+    const gallerySlides = document.querySelectorAll(".gallery__slides");
+    const pageSlider = document.querySelector(".gallery__page-slider");
+    const mainSlider = document.querySelector(".gallery__main-slider");
     // +1 к счётчику товаров по нажатию
     plus.addEventListener("click", () => {
-        let scoreText = +score.textContent;
-        score.textContent = ++scoreText;
+        let scoreText = +score.value;
+        score.value = ++scoreText;
     });
     // -1 к счётчику товаров по нажатию
     minus.addEventListener("click", () => {
-        let scoreText = +score.textContent;
+        let scoreText = +score.value;
         if (scoreText > 1) {
-            score.textContent = --scoreText;
+            score.value = --scoreText;
         }
     });
+    // Валидация счётчика товаров ---------------------------------------------------------------
+    score.addEventListener("input", (e) => {
+        const isNumber = /^\d*$/.test(e.target.value);
+        if (!isNumber) {
+            e.target.value = e.target.value.replace(/\D/g, "");
+        }
+    });
+    score.addEventListener("blur", (e) => {
+        if (!e.target.value || e.target.value == 0) {
+            e.target.value = 1;
+        } else {
+            e.target.value = +e.target.value;
+        }
+    })
     // Включение счётчика корзины при загрузке страницы ---------------------------------------------------------------
     function addCartScore() {
         if (+cartScore.textContent > 0) {
@@ -82,6 +99,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // Добавление товаров в корзину ---------------------------------------------------------------
     // Функция создания товара
     function createGoodItem(scoreText = 4) {
+        let cost = +document.querySelector(".price__cost").textContent.slice(1,-3);
         goodsList.innerHTML =
             `<div class="goods__item">
                 <div class="goods__picture">
@@ -90,8 +108,8 @@ window.addEventListener("DOMContentLoaded", () => {
                 <div class="goods__text">
                     <p class="goods__title">Fall Limited Edition Sneakers</p>
                     <div class="goods__price">
-                        <p class="goods__start-cost">$125.00 x ${scoreText}</p>
-                        <p class="goods__final-cost">$375.00</p>
+                        <p class="goods__start-cost">$${cost} x ${scoreText}</p>
+                        <p class="goods__final-cost">$${cost*scoreText}</p>
                     </div>
                 </div>
                 <div class="goods__bin">
@@ -110,13 +128,92 @@ window.addEventListener("DOMContentLoaded", () => {
     }
     // Добавление товара в корзину по клику на кнопку
     addBtn.addEventListener("click", () => {
-        let scoreText = +score.textContent;
+        let scoreText = +score.value;
         let cartScoreText = +cartScore.textContent;
         cartScore.textContent = cartScoreText + scoreText;
         cartScore.style.display = "block";
         createGoodItem(cartScoreText + scoreText);
-        const bin = document.querySelector('.goods__bin');
+        const bin = document.querySelector(".goods__bin");
         bin.addEventListener("click", removeGoodItem);
         addCart();
+    });
+    // Галерея thubmnails ---------------------------------------------------------------
+    const lastActiveThumbnail =
+    {
+        src: "http://127.0.0.1:5500/images/gallery/image-product-1.jpg",
+        index: 0
+    };
+    // Выключение других активных thumbnail
+    function offActiveThumbnails(arr,target,...activeClasses) {
+        arr.forEach((i,index) => {
+            if (i.children[0] != target) {
+                i.classList.remove(activeClasses[0]);
+                i.children[0].classList.remove(activeClasses[1]);
+            } else {
+                lastActiveThumbnail.index = index;
+            }
+        });
+        return lastActiveThumbnail;
+    }
+    // Включение активного thumbnail
+    function onActiveThumbnail(mainPhoto,target,...activeClasses) {
+        mainPhoto.src = target.src;
+        target.parentElement.classList.add(activeClasses[0]);
+        target.classList.add(activeClasses[1]);
+        lastActiveThumbnail.src = mainPhoto.src;
+        return lastActiveThumbnail;
+    }
+    // Передача данных от одного слайдера другому
+    function transferData(slider) {
+        const galleryPhoto = slider.querySelector(".gallery__photo");
+        const gallerySlides = Array.from(slider.querySelector(".gallery__slides").children);
+        const lastTarget = gallerySlides[lastActiveThumbnail.index].children[0];
+        galleryPhoto.src = lastActiveThumbnail.src;
+        onActiveThumbnail(
+            galleryPhoto,
+            lastTarget,
+            "gallery__min-img_type_active",
+            "gallery__thumb_type_active"
+        );
+        offActiveThumbnails(
+            gallerySlides,
+            lastTarget,
+            "gallery__min-img_type_active",
+            "gallery__thumb_type_active"
+        );
+    }
+    gallerySlides.forEach(item => {
+        item.addEventListener("click", (e) => {
+            const slider = item.parentElement;
+            // Получение главного фото слайдера
+            const galleryPhoto = slider.querySelector(".gallery__photo");
+            const galleryChildren = Array.from(e.currentTarget.children);
+            // Включение активного thumbnail
+            if (e.target.classList.contains("gallery__thumb")){
+                onActiveThumbnail(
+                    galleryPhoto,
+                    e.target,
+                    "gallery__min-img_type_active",
+                    "gallery__thumb_type_active"
+                );
+                offActiveThumbnails(
+                    galleryChildren,
+                    e.target,
+                    "gallery__min-img_type_active",
+                    "gallery__thumb_type_active"
+                );
+            }
+        });
+    });
+    pageSlider.addEventListener("click", (e) => {
+        if (e.target.classList.contains("gallery__photo")) {
+            mainSlider.style.display = "block";
+            const cross = mainSlider.querySelector(".gallery__cross");
+            transferData(mainSlider);
+            cross.addEventListener("click", () => {
+                mainSlider.style.display = "";
+                transferData(pageSlider);
+            });
+        }
     });
 });
